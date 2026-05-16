3edc5a1592b13902f0e49b0bb090c1f8429c0d3d
ajstrick81 <adamjstricker@gmail.com>
2026-05-16 18:37:19 -0500

    Delete patches/src/main/kotlin/patches/androidtv/Fingerprints.kt

--- patches/src/main/kotlin/patches/androidtv/Fingerprints.kt
+++
@@ -1,9 +0,0 @@
-package patches.androidtv
-
-import patches.core.ParameterizedFingerprint
-
-object HideAdsFingerprint : ParameterizedFingerprint(
-    "invoke-static {.*}, Lcom/google/ads/interactivemedia/v3/impl/data/zzbr;->create\\(Landroid/net/Uri;Ljava/lang/String;Ljava/lang/String;Lcom/google/ads/interactivemedia/v3/internal/zzpm;\\)Lcom/google/ads/interactivemedia/v3/impl/data/zzbr;",
-    "move-result-object (\\w+)",
-    "invoke-virtual {.*}, Lcom/google/ads/interactivemedia/v3/internal/zzew;->zzb\\(Landroid/content/Context;Lcom/google/ads/interactivemedia/v3/impl/data/zzbr;Lcom/google/ads/interactivemedia/v3/impl/data/TestingConfiguration;Ljava/util/concurrent/ExecutorService;\\)Lcom/google/ads/interactivemedia/v3/internal/zzev;"
-)
