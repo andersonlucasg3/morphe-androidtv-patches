f6e520aa0d3a7d2a040cd1a0008483db80cd4868
ajstrick81 <adamjstricker@gmail.com>
2026-05-17 18:35:31 -0500

    Update settings.gradle.kts with JitPack comment
    Added comment for JitPack dependency resolution.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,12 @@ rootProject.name = "morphe-androidtv-patches"
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
     repositories {
         google()
         mavenCentral()
+
+        // Add this next if Morphe dependency fails to resolve
+        // maven { url = uri("https://jitpack.io") }
     }
 }
