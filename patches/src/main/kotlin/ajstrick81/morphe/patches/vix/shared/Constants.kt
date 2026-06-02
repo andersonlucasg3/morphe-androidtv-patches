9ef96c642ac637769355dfc67f44f16d0643d429
ajstrick81 <adamjstricker@gmail.com>
2026-06-02 12:22:01 -0500

    Add Constants.kt with package and version information

---
+++ patches/src/main/kotlin/ajstrick81/morphe/patches/vix/shared/Constants.kt
@@ -0,0 +1,6 @@
+package ajstrick81.morphe.patches.vix.shared
+
+internal const val PACKAGE_NAME = "com.univision.prendetv"
+internal const val COMPATIBLE_VERSION = "4.46.0_tv"
+
+internal val COMPATIBILITY = PACKAGE_NAME to setOf(COMPATIBLE_VERSION)
