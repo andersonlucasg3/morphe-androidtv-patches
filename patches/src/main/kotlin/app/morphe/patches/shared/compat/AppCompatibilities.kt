ac43dd1dbb4c92abc96f717beb5c1fc8c944dd1f
ajstrick81 <adamjstricker@gmail.com>
2026-06-14 12:37:23 -0500

    fix: new version push by adding target version to PARAMOUNT_TV compatibility
    Updated PARAMOUNT_TV compatibility to include additional target version.

--- patches/src/main/kotlin/app/morphe/patches/shared/compat/AppCompatibilities.kt
+++ patches/src/main/kotlin/app/morphe/patches/shared/compat/AppCompatibilities.kt
@@ -6,11 +6,14 @@ import app.morphe.patcher.patch.Compatibility
 object AppCompatibilities {
 
     val PARAMOUNT_TV = Compatibility(
-        name = "Paramount+ Android TV",
-        packageName = "com.cbs.ott",
-        appIconColor = 0x0064FF,
-        targets = listOf(AppTarget("16.12.0")),
-    )
+    name = "Paramount+ Android TV",
+    packageName = "com.cbs.ott",
+    appIconColor = 0x0064FF,
+    targets = listOf(
+        AppTarget("16.8.0"),
+        AppTarget("16.12.0"),
+       ),
+   )
 
     val DISNEY_PLUS_TV = Compatibility(
         name = "Disney+ Android TV",
